9be47ba9060a3b13297bd7425da41a186596c6c3
IvanCS-Chenfu <152430268+IvanCS-Chenfu@users.noreply.github.com>
2024-12-29 13:58:59 +0100

    Update Emisor_Uno.ino

--- ESP-NOW/Emisor/Emisor_Uno.ino
+++ ESP-NOW/Emisor/Emisor_Uno.ino
@@ -3,19 +3,22 @@
 #include <esp_wifi.h>
 #include <WiFi.h>
 
-uint8_t macDestino[] = {0x24, 0x6F, 0x28, 0x12, 0x34, 0x56}; // Reemplaza con la MAC de la placa esclava
+uint8_t macDestino[] = {0xC8, 0x2E, 0x18, 0xF8, 0x58, 0x94}; // Reemplaza con la MAC de la placa esclava
 
 // Estructura para el mensaje
-typedef struct {
+typedef struct 
+{
   char mensaje[32];  // Mensaje que se enviará
 } mensaje_t;
 
 mensaje_t datosEnviar;
 
 // Callback para verificar el envío
-void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) 
+{
   Serial.print("Envío a ");
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < 6; i++) 
+  {
     Serial.printf("%02X", mac_addr[i]);
     if (i < 5) Serial.print(":");
   }
@@ -23,7 +26,8 @@ void onDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Éxito" : "Fallo");
 }
 
-void setup() {
+void setup() 
+{
   Serial.begin(115200);
 
   // Configuración WiFi
@@ -32,7 +36,8 @@ void setup() {
   Serial.println(WiFi.macAddress());
 
   // Inicializar ESP-NOW
-  if (esp_now_init() != ESP_OK) {
+  if (esp_now_init() != ESP_OK) 
+  {
     Serial.println("Error al inicializar ESP-NOW");
     return;
   }
@@ -45,7 +50,8 @@ void setup() {
   memcpy(peerInfo.peer_addr, macDestino, 6);
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
-  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) 
+  {
     Serial.println("Error al agregar el peer");
     return;
   }
@@ -54,13 +60,17 @@ void setup() {
   strcpy(datosEnviar.mensaje, "¡Hola ESP-Now Esclavo!");
 }
 
-void loop() {
+void loop() 
+{
   // Enviar datos cada 2 segundos
   esp_err_t result = esp_now_send(macDestino, (uint8_t *)&datosEnviar, sizeof(datosEnviar));
-  if (result == ESP_OK) {
+  if (result == ESP_OK) 
+  {
     Serial.println("Mensaje enviado correctamente");
-  } else {
-    Serial.println("Error al enviar el mensaje");
+  }
+  else
+  {
+    Serial.println("Error al enviar el mensaje"); 
   }
 
   delay(2000);
